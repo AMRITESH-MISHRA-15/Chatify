25f04b745af85b7cc8e80964719a56956d050f8d
AMRITESH-MISHRA-15 <amriteshmishra2004@gmail.com>
2026-02-10 22:35:08 +0530

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
@@ -15,6 +15,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
